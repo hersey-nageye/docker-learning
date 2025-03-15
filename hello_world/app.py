88720aa5eae3e9561e718a8f695021c0422f5f5b
hersey-nageye <hersey_nageye1@hotmail.com>
2025-03-15 10:07:44 +0000

    Fixed errors preventing app from working

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,18 +1,17 @@
 # app.py
 
 from flask import Flask
-import mysql.connector
-import os
+import MySQLdb
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     # Connect to the MySQL database
-    db = mysql.connector.connect(
+    db = MySQLdb.connect(
         host="mydb",    # Hostname of the MySQL container
         user="root",    # Username to connect to MySQL
-        passwd=os.getenv("MYSQL_ROOT_PASSWORD"),  # Password for the MySQL user
+        passwd="my-secret-pw",  # Password for the MySQL user
         db="mysql"      # Name of the database to connect to
     )
     cur = db.cursor()
@@ -21,4 +20,4 @@ def hello_world():
     return f'Hello, World! MySQL version: {version[0]}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
